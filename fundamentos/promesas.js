cc5b4fb6d0c68fe45e396c47589024029779718d
Tania <tanialibramg@gmail.com>
2018-11-21 18:25:49 +0100

    Create promesas.js

---
+++ fundamentos/promesas.js
@@ -0,0 +1,18 @@
+let empleados = [{
+    id: 1,
+    nombre: "Tania"
+},{
+    id: 2,
+    nombre: "Marta"
+},{
+    id: 3,
+    nombre: "Jorge"
+}];
+
+let salarios = [{
+    id: 1,
+    importe: "1000"
+},{
+    id: 2,
+    importe: "900"
+}];
