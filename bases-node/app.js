a2be558e71ee2277cf41d2d49c0c4b30bdedfb90
Tania <tanialibramg@gmail.com>
2018-12-08 16:44:07 +0100

    fs
    fs writefile

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
 let base = 2;
+let data = '';
 
 for (let i=1;i<=10;i++){
-  console.log(`${base} * ${i} = ${base*i}`);
+  data += `${base} * ${i} = ${base*i}\n`;
 }
+
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+  if (err) throw err;
+
+  console.log('El archivo ha sido creado');
+});
