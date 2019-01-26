80f20933939b4e881d0e59ffced078904a2f98c4
Tania <tanialibramg@gmail.com>
2019-01-26 12:50:37 +0100

    parametros por linea comandos

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,6 +1,11 @@
 const {crearArchivo} = require('./multiplicar/multiplicar');
 
-let base = 4;
+//para ver los argumentos recibidos
+//console.log(process.argv);
+
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1];
 
 crearArchivo(base)
   .then(archivo=>console.log(`Archivo creado: ${archivo}`))
