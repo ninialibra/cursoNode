ccac837561fb2e10e83b58e3f2960b4bd1fc8ed6
Tania <tanialibramg@gmail.com>
2019-01-27 20:48:44 +0100

    yargs

--- bases-node/multiplicar/multiplicar.js
+++ bases-node/multiplicar/multiplicar.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite=10) => {
+
+  for (let i=1;i<=limite;i++){
+    console.log(`${base} * ${i} = ${base*i}`);
+  }
+
+}
+
+let crearArchivo = (base, limite=10) => {
   return new Promise ((resolve, reject) => {
 
     if(!Number(base)){
@@ -10,7 +18,7 @@ let crearArchivo = (base) => {
 
     let data = '';
 
-    for (let i=1;i<=10;i++){
+    for (let i=1;i<=limite;i++){
       data += `${base} * ${i} = ${base*i}\n`;
     }
 
@@ -25,5 +33,6 @@ let crearArchivo = (base) => {
 }
 
 module.exports = {
-  crearArchivo
+  crearArchivo,
+  listarTabla
 }
