e2a535c225a187a7c6de432c27d43aacaf313a9a
Tania <tanialibramg@gmail.com>
2019-01-28 17:14:54 +0100

    colors

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,28 +1,10 @@
-const argv = require('yargs')
-                    .command('listar', 'Imprime en consola la tabla de multiplicar',{
-                      base: {
-                        demand: true,
-                        alias: 'b'
-                      },
-                      limite: {
-                        alias: 'l',
-                        default: 10
-                      }
-                    })
-                    .command('crear', 'Crea el archivo con la tabla de multiplicar',{
-                      base: {
-                        demand: true,
-                        alias: 'b'
-                      },
-                      limite: {
-                        alias: 'l',
-                        default: 10
-                      }
-                    })
-                    .help()
-                    .argv;
+const argv = require("./config/yargs").argv;
+const colors = require("colors/safe");
 
-const {crearArchivo, listarTabla} = require('./multiplicar/multiplicar');
+const {
+  crearArchivo,
+  listarTabla
+} = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
 
@@ -31,10 +13,10 @@ switch (comando) {
     listarTabla(argv.base, argv.limite);
     break;
   case "crear":
-      crearArchivo(argv.base, argv.limite)
-        .then(archivo=>console.log(`Archivo creado: ${archivo}`))
-        .catch(e=>console.log(e));
-      break;
+    crearArchivo(argv.base, argv.limite)
+      .then(archivo => console.log(`Archivo creado: ` + colors.green(archivo)))
+      .catch(e => console.log(e));
+    break;
   default:
     console.log('Comando no reconocido');
 
--- bases-node/multiplicar/multiplicar.js
+++ bases-node/multiplicar/multiplicar.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
+const colors = require("colors");
 
 let listarTabla = (base, limite=10) => {
+
+  console.log("==========================".green);
+  console.log(`Tabla del ${base} con límite de ${limite}`.green);
+  console.log("==========================".green);
 
   for (let i=1;i<=limite;i++){
     console.log(`${base} * ${i} = ${base*i}`);
