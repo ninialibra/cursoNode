f4342257353f2df13cc9b67d65878ef55280c3ce
Tania <tanialibramg@gmail.com>
2019-03-07 18:52:26 +0100

    update server

--- restserver/server/server.js
+++ restserver/server/server.js
@@ -46,6 +46,6 @@ app.delete('/usuario', function(req, res) {
   res.json('deleteUsuario')
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Escuchando el puerto:", process.env.PORT);
 });
