8bf9bb34ffa8b64bf66ef785b9cc2eb8556baba0
Tania Miranda <tanialibramg@gmail.com>
2019-06-15 14:26:04 +0200

    mysql conexion

--- typescript-mysql/src/index.ts
+++ typescript-mysql/src/index.ts
@@ -1,6 +1,12 @@
 import Server from './server/server';
+import router from './rutas/rutas';
+import MySQL from './mysql/mysql';
 
 const server = Server.init(3000);
+server.app.use(router);
+
+const mysql = new MySQL();
+
 server.start(()=>{
     console.log('Servidor corriendo en puerto 3000');    
 });
