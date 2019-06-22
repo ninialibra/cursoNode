2fa1fd32f52ae68ca85830fe0e26680cf23dd14f
Tania Miranda <tanialibramg@gmail.com>
2019-06-22 10:57:16 +0200

    typescript finalizado

--- typescript-mysql/src/rutas/rutas.ts
+++ typescript-mysql/src/rutas/rutas.ts
@@ -1,12 +1,29 @@
 import {Router, Request, Response} from 'express';
+import MySQL from '../mysql/mysql';
 
 const router = Router();
 
 router.get('/heroes', (req: Request, res: Response)=>{
 
-    res.json({
-        ok: true,
-        mensaje: 'Todo está bien'
+    const query = `
+        SELECT *
+        FROM heroes
+    `;
+
+    MySQL.ejecutarQuery(query, (error:any, heroes:Object[])=>{
+
+        if(error){
+            res.status(400).json({
+                ok: false,
+                error
+            });
+        }else{
+
+            res.json({
+                ok: true,
+                heroes
+            });
+        }
     });
 
 });
@@ -14,11 +31,28 @@ router.get('/heroes', (req: Request, res: Response)=>{
 router.get('/heroes/:id', (req: Request, res: Response)=>{
 
     const id = req.params.id;
+    const escapeID = MySQL.instance.conexion.escape(id);
 
-    res.json({
-        ok: true,
-        mensaje: 'Todo está bien',
-        id
+    const query = `
+        SELECT *
+        FROM heroes
+        WHERE id = ${escapeID}
+    `;
+
+    MySQL.ejecutarQuery(query, (error:any, heroe:Object[])=>{
+
+        if(error){
+            res.status(400).json({
+                ok: false,
+                error
+            });
+        }else{
+
+            res.json({
+                ok: true,
+                heroe
+            });
+        }
     });
 
 });
